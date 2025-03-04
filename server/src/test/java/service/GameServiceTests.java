1226d71bda2e7baf859b1e77c7999fabb494b83e
EntropyValley <github@itsval.org>
2025-03-04 00:10:03 -0700

    Add Custom Unit Tests

---
+++ server/src/test/java/service/GameServiceTests.java
@@ -0,0 +1,273 @@
+package service;
+
+import chess.ChessGame;
+import dataaccess.*;
+
+import exceptions.*;
+import model.AuthData;
+import model.GameData;
+
+import java.util.HashSet;
+import java.util.UUID;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class GameServiceTests {
+    private static GameDAO gameDAO;
+    private static AuthDAO authDAO;
+
+    private GameService gameService;
+
+    @BeforeAll
+    static void init() {
+        gameDAO = new GameDAOMem();
+        authDAO = new AuthDAOMem();
+    }
+
+    @BeforeEach
+    void initIndividual() {
+        gameService = new GameService(gameDAO, authDAO);
+    }
+
+    @AfterEach
+    void destroy() {
+        try {
+            gameDAO.clear();
+            authDAO.clear();
+        } catch (DataAccessException exception) {
+            System.out.println("Error clearing gameDAO or authDAO");
+        }
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Normal List Games")
+    void list_normal() {
+        String authToken = UUID.randomUUID().toString();
+        try {
+            authDAO.addAuth(new AuthData("testUser", authToken));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        GameData game1 = new GameData(1, null, null, "Game 1", new ChessGame());
+        GameData game2 = new GameData(2, null, null, "Game 2", new ChessGame());
+        GameData game3 = new GameData(3, null, null, "Game 3", new ChessGame());
+
+        try {
+            gameDAO.createGame(game1);
+            gameDAO.createGame(game2);
+            gameDAO.createGame(game3);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        HashSet<GameData> retrievedGames;
+        try {
+            retrievedGames = gameService.listGames(authToken);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(3, retrievedGames.size());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Bad Auth List Games")
+    void list_no_auth() throws DataAccessException {
+        String authToken = UUID.randomUUID().toString();
+
+        GameData game1 = new GameData(1, null, null, "Game 1", new ChessGame());
+        GameData game2 = new GameData(2, null, null, "Game 2", new ChessGame());
+        GameData game3 = new GameData(3, null, null, "Game 3", new ChessGame());
+
+        try {
+            gameDAO.createGame(game1);
+            gameDAO.createGame(game2);
+            gameDAO.createGame(game3);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            gameService.listGames(authToken);
+        } catch (UnauthorizedException e) {
+            return;
+        }
+
+        fail("UnauthorizedException not thrown");
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Normal Create Game")
+    void create_normal() {
+        String authToken = UUID.randomUUID().toString();
+        try {
+            authDAO.addAuth(new AuthData("testUser", authToken));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            gameService.createGame(authToken, "New Game");
+        } catch (NoIDAvailableException | BadRequestException e) {
+            throw new RuntimeException(e);
+        }
+
+        HashSet<GameData> retrievedGames;
+
+        try {
+            retrievedGames = gameService.listGames(authToken);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(1, retrievedGames.size());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Null Name Create Game")
+    void create_null() {
+        String authToken = UUID.randomUUID().toString();
+        try {
+            authDAO.addAuth(new AuthData("testUser", authToken));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            gameService.createGame(authToken, null);
+        } catch (NoIDAvailableException e) {
+            throw new RuntimeException(e);
+        } catch (BadRequestException e) {
+            return;
+        }
+
+        fail("BadRequestException not thrown");
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Normal Join Game")
+    void join_normal()  {
+        String authToken = UUID.randomUUID().toString();
+        try {
+            authDAO.addAuth(new AuthData("testUser", authToken));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        GameData game = new GameData(1, null, null, "Game 1", new ChessGame());
+        try {
+            gameDAO.createGame(game);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            gameService.joinGame(authToken, 1, ChessGame.TeamColor.WHITE);
+        } catch (ColorTakenException | DataAccessException | GameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        GameData retrievedGame;
+        try {
+            retrievedGame = gameDAO.findGame(1);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(new GameData(1, "testUser", null, "Game 1", new ChessGame()), retrievedGame);
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Already Taken Join Game")
+    void join_already_taken() {
+        String authToken = UUID.randomUUID().toString();
+        try {
+            authDAO.addAuth(new AuthData("testUser1", authToken));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        GameData game = new GameData(1, null, null, "Game 1", new ChessGame());
+        try {
+            gameDAO.createGame(game);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            gameService.joinGame(authToken, 1, ChessGame.TeamColor.WHITE);
+        } catch (ColorTakenException | DataAccessException | GameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String authToken2 = UUID.randomUUID().toString();
+        try {
+            authDAO.addAuth(new AuthData("testUser2", authToken2));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            gameService.joinGame(authToken2, 1, ChessGame.TeamColor.WHITE);
+        } catch (DataAccessException | GameNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (ColorTakenException e) {
+            return;
+        }
+
+        fail("ColorTakenException not thrown");
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Clear")
+    void clear() {
+        String authToken = UUID.randomUUID().toString();
+        try {
+            authDAO.addAuth(new AuthData("testUser", authToken));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        GameData game = new GameData(1, null, null, "Game 1", new ChessGame());
+
+        try {
+            gameDAO.createGame(game);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            gameService.clear();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        HashSet<GameData> storedGames;
+        try {
+            storedGames = gameDAO.requestGames();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertTrue(storedGames.isEmpty());
+
+        try {
+            authDAO.getAuth(authToken);
+        } catch (DataAccessException e) {
+            return;
+        }
+
+        fail("UserData not cleared");
+    }
+}
---
+++ server/src/test/java/service/UserServiceTests.java
@@ -0,0 +1,194 @@
+package service;
+
+import dataaccess.*;
+import exceptions.UnauthorizedException;
+import exceptions.UsernameTakenException;
+import model.AuthData;
+import model.UserData;
+
+import org.junit.jupiter.api.*;
+
+import java.util.UUID;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class UserServiceTests {
+    private static UserDAO userDAO;
+    private static AuthDAO authDAO;
+
+    private UserService userService;
+
+    @BeforeAll
+    static void init() {
+        userDAO = new UserDAOMem();
+        authDAO = new AuthDAOMem();
+    }
+
+    @BeforeEach
+    void initIndividual() {
+        userService = new UserService(userDAO, authDAO);
+    }
+
+    @AfterEach
+    void destroy() {
+        try {
+            userDAO.clear();
+            authDAO.clear();
+        } catch (DataAccessException exception) {
+            System.out.println("Error clearing userDAO or authDAO");
+        }
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Normal Register")
+    void register_normal() {
+        UserData userData = new UserData("test1", "test2", "test3");
+        AuthData authData;
+        try {
+            authData = userService.register(userData);
+        } catch (UsernameTakenException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals("test1", authData.username());
+        assertNotNull(authData.authToken());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Duplicate Username Register")
+    void register_duplicate() {
+        UserData userData = new UserData("test1", "test2", "test3");
+
+        try {
+            userService.register(userData);
+        } catch (UsernameTakenException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            userService.register(userData);
+        } catch (UsernameTakenException e) {
+            return;
+        }
+
+        fail("UsernameTakenException not thrown");
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Normal Login")
+    void login_normal() {
+        UserData userData = new UserData("test1", "test2", "test3");
+        try {
+             userService.register(userData);
+        } catch (UsernameTakenException e) {
+            throw new RuntimeException(e);
+        }
+
+        AuthData authData2;
+        try {
+            authData2 = userService.login(userData);
+        } catch (UnauthorizedException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals("test1", authData2.username());
+        assertNotNull(authData2.authToken());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Invalid Login")
+    void login_invalid() {
+        UserData userData = new UserData("test1", "test2", "test3");
+
+        try {
+            userService.login(userData);
+        } catch (UnauthorizedException e) {
+            return;
+        }
+
+        fail("UnauthorizedException not thrown");
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Normal Logout")
+    void logout_normal() {
+        UserData userData = new UserData("test1", "test2", "test3");
+        AuthData authData;
+        try {
+            authData = userService.register(userData);
+        } catch (UsernameTakenException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            userService.logout(authData.authToken());
+        } catch (UnauthorizedException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            authDAO.getAuth(authData.authToken());
+        } catch (DataAccessException e) {
+            return;
+        }
+
+        fail("AuthData still exists");
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Invalid Token Logout")
+    void logout_invalid_token() {
+        try {
+            userService.logout(UUID.randomUUID().toString());
+        } catch (UnauthorizedException e) {
+            return;
+        }
+
+        fail("Logged Out Nonexistent Session");
+    }
+
+
+    @Test
+    @Order(7)
+    @DisplayName("Clear")
+    void clear() {
+        UserData userData = new UserData("test1", "test2", "test3");
+
+        AuthData initialAuth;
+        try {
+            initialAuth = userService.register(userData);
+        } catch (UsernameTakenException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            userService.clear();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        boolean caught = false;
+        try {
+            userDAO.getUser("test1");
+        } catch (DataAccessException e) {
+            caught = true;
+        }
+
+        assertTrue(caught);
+
+        caught = false;
+        try {
+            authDAO.getAuth(initialAuth.authToken());
+        } catch (DataAccessException e) {
+            caught = true;
+        }
+
+        assertTrue(caught);
+    }
+}
