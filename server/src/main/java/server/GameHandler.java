32802dd2342d054b87161305c6bc13321a53f87f
EntropyValley <github@itsval.org>
2025-03-12 17:08:57 -0600

    Finish repairing repository

--- server/src/main/java/server/GameHandler.java
+++ server/src/main/java/server/GameHandler.java
@@ -62,7 +62,7 @@ public class GameHandler {
         return "{\"gameID\": " + gameID + "}";
     }
 
-    public Object _join(
+    public Object hJoin(
             Request request,
             Response response
     ) throws BadRequestException, ColorTakenException, DataAccessException, GameNotFoundException {
