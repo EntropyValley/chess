0961115767433dcc41ad7885f3127d1445c4cbf0
EntropyValley <github@itsval.org>
2025-04-01 22:16:10 -0600

    add rest of request methods to ServerFacade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,7 +1,10 @@
 package client;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import model.AuthData;
+import model.GameData;
+import model.UserData;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -9,7 +12,6 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 public class ServerFacade {
 
@@ -61,9 +63,86 @@ public class ServerFacade {
             }
             successReader.close();
 
-            return new Gson().fromJson(successBody.toString(), responseClass);
+            if (responseClass != null) {
+                return new Gson().fromJson(successBody.toString(), responseClass);
+            } else {
+                return null;
+            }
         } catch (Exception exception) {
             throw new ResponseException(-1, exception.getMessage());
+        }
+    }
+
+    public void clear() throws ResponseException {
+        this.request("DELETE", "/db", null, null, null);
+    }
+
+    public boolean register(UserData userData) throws ResponseException {
+        try {
+            this.request("POST", "/user", userData, AuthData.class, null);
+            return true;
+        } catch (Exception exception) {
+            System.out.println("User " + userData.username() + " already exists");
+            return false;
+        }
+    }
+
+    public AuthData login(UserData userData) throws ResponseException {
+        try {
+            return this.request("POST", "/session", userData, AuthData.class, null);
+        } catch (Exception exception) {
+            System.out.println("Could not login user " + userData.username());
+            return null;
+        }
+    }
+
+    public void logout(AuthData authData) throws ResponseException {
+        try {
+            this.request("DELETE", "/session", null, null, authData);
+        } catch (Exception exception) {
+            System.out.println("Unable to logout user");
+        }
+    }
+
+    public GameData[] listGames(AuthData authData) throws ResponseException {
+        record Games(GameData[] games) {}
+        try {
+            var response = this.request("GET", "/game", null, Games.class, authData);
+
+            if (response != null) {
+                return response.games();
+            }
+            return null;
+        } catch (Exception exception) {
+            System.out.println("Unable to fetch games list");
+            return null;
+        }
+    }
+
+    public boolean createGame(AuthData authData, String gameName) throws ResponseException {
+        JsonObject request = new JsonObject();
+        request.addProperty("gameName", gameName);
+
+        try {
+            this.request("POST", "/game", request, GameData.class, authData);
+            return true;
+        } catch (Exception exception) {
+            System.out.println("Unable to create game");
+            return false;
+        }
+    }
+
+    public boolean joinGame(AuthData authData, int id, String color) throws ResponseException {
+        JsonObject request = new JsonObject();
+        request.addProperty("gameID", id);
+        request.addProperty("playerColor", color);
+
+        try {
+            this.request("PUT", "/game", request, GameData.class, authData);
+            return true;
+        } catch (Exception exception) {
+            System.out.println("Unable to join game");
+            return false;
         }
     }
 }
