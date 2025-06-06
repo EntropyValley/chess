8ee8f38925c9babddeaedcafe588cd854306ec89
EntropyValley <github@itsval.org>
2025-06-05 18:58:36 -0600

    Create Command and Message Classes for serializable communication between server and client.

---
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -0,0 +1,13 @@
+package websocket.commands;
+
+import chess.ChessMove;
+
+public class MakeMoveCommand extends UserGameCommand {
+    ChessMove chessMove;
+
+    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID, ChessMove chessMove) {
+        super(commandType, authToken, gameID);
+        this.chessMove = chessMove;
+    }
+
+}
