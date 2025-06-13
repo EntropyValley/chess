71dd58393dbc5854d89e26c5f2f64acb1691a02f
EntropyValley <github@itsval.org>
2025-06-12 19:42:59 -0700

    Add WebSocketFacade command hooks

--- client/src/main/java/websocket/WebSocketFacade.java
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -1,6 +1,9 @@
 package websocket;
 
+import chess.ChessMove;
 import client.ResponseException;
+import model.AuthData;
+import websocket.commands.*;
 import websocket.messages.ServerMessage;
 
 import javax.websocket.*;
@@ -20,28 +23,58 @@ public class WebSocketFacade extends Endpoint {
             WebSocketContainer wsContainer = ContainerProvider.getWebSocketContainer();
             session = wsContainer.connectToServer(this, wsURI);
 
-            session.addMessageHandler(new MessageHandler.Whole<String>() {
-                @Override
-                public void onMessage(String msg) {
-                    ServerMessage serverMessage = new Gson().fromJson(msg, ServerMessage.class);
+            session.addMessageHandler((MessageHandler.Whole<String>) msg -> {
+                ServerMessage serverMessage = new Gson().fromJson(msg, ServerMessage.class);
 
-                    switch(serverMessage.getServerMessageType()) {
-                        case NOTIFICATION -> System.out.println("NOTIFICATION");
-                        case ERROR -> System.out.println("ERROR");
-                        case LOAD_GAME -> System.out.println("LOAD");
-                        case null -> System.out.println("Invalid Server Message received...");
-                    }
+                switch(serverMessage.getServerMessageType()) {
+                    case NOTIFICATION -> System.out.println("NOTIFICATION");
+                    case ERROR -> System.out.println("ERROR");
+                    case LOAD_GAME -> System.out.println("LOAD");
+                    case null -> System.out.println("Invalid Server Message received...");
                 }
             });
         } catch (URISyntaxException exception) {
-
+            System.out.println("Invalid Server URI, please reconfigure...");
         } catch (DeploymentException exception) {
-
+            System.out.println("Error connecting to websocket...");
         } catch (IOException exception) {
+            System.out.println("Error communicating with websocket server...");
         }
     }
 
     @Override
     public void onOpen(Session session, EndpointConfig configuration) {
+    }
+
+    public void sendCommand(UserGameCommand command)  {
+        try {
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        } catch (IOException exception) {
+            System.out.println("Unable to send command to server...");
+        }
+    }
+
+    public void connect(AuthData authData, Integer gameID) {
+        sendCommand(
+            new UserGameCommand(UserGameCommand.CommandType.CONNECT, authData.authToken(), gameID)
+        );
+    }
+
+    public void makeMove(AuthData authData, Integer gameID, ChessMove chessMove) {
+        sendCommand(
+            new MakeMoveCommand(UserGameCommand.CommandType.MAKE_MOVE, authData.authToken(), gameID, chessMove)
+        );
+    }
+
+    public void resign(AuthData authData, Integer gameID) {
+        sendCommand(
+            new UserGameCommand(UserGameCommand.CommandType.RESIGN, authData.authToken(), gameID)
+        );
+    }
+
+    public void leave(AuthData authData, Integer gameID) {
+        sendCommand(
+            new UserGameCommand(UserGameCommand.CommandType.LEAVE, authData.authToken(), gameID)
+        );
     }
 }
